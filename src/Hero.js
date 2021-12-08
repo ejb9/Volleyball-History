03a60c21247abd92568c22206b58d720749a9886
Emily Braudaway <Emily.Braudaway@sckans.edu>
2021-12-08 11:36:20 -0600

    edit

--- src/Hero.js
+++ src/Hero.js
@@ -1,11 +1,6 @@
-import HomeUplefttrianlge from '../home-uplefttriangle.svg' ;
-import HomeUprighttriangle from '../home-uprighttriangle.svg' ; 
-
 const Hero = () => {
     return (
        <>
-            <img src={HomeUplefttrianlge} />
-            <img src={HomeUprighttriangle} />
         </>
     );
 };
